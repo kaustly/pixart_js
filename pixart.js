4407068d966441374cb1c87558bb1766dcdac89d
Adam Bray <adam.bray@yale.edu>
2015-04-29 11:29:56 -0400

    cleaned up code to use named variables and functions

--- pixart.js
+++ pixart.js
@@ -1,7 +1,14 @@
 // When I click the "Set Color" button, it should change the color of the
 // "brush" box to the color I specify in the input field.
-document.querySelector("#set-color").addEventListener("click", function(event) {
+
+var button = document.querySelector("#set-color");
+var colorField = document.querySelector("#color-field");
+var brush = document.querySelector(".brush");
+
+function setBrushColor(event) {
       event.preventDefault();
-      var newColor = document.querySelector("#color-field").value;
-      document.querySelector(".brush").style.backgroundColor = newColor;
-});
+      var newColor = colorField.value;
+      brush.style.backgroundColor = newColor;
+}
+
+button.addEventListener("click", setBrushColor);
