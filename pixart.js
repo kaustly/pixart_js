cd5ac831e9607ec77279c084ebba0e825192cbb8
Adam Bray <adam.bray@yale.edu>
2015-04-30 11:39:36 -0400

    cleaned up code

--- pixart.js
+++ pixart.js
@@ -1,10 +1,14 @@
-// When I click the "Set Color" button, it should change the color of the
-// "brush" box to the color I specify in the input field.
-
 var button = document.querySelector("#set-color");
 var colorField = document.querySelector("#color-field");
 var brush = document.querySelector(".brush");
 var color = "#1B4370";
+
+startPixart();
+
+function startPixart() {
+  button.addEventListener("click", setBrushColor);
+  makeCanvas();
+}
 
 function setBrushColor(event) {
       event.preventDefault();
@@ -13,14 +17,15 @@ function setBrushColor(event) {
       color = newColor;
 }
 
-button.addEventListener("click", setBrushColor);
-
-for(var i = 0; i < 8000; i++) {
-  var newDiv = document.createElement("div");
-  newDiv.classList.add("square");
-  newDiv.addEventListener("mouseover", function(){ this.style.backgroundColor = color; })
-  document.querySelector("body").appendChild(newDiv);
+function makeCanvas() {
+  for(var i = 0; i < 8000; i++) {
+    var newDiv = document.createElement("div");
+    newDiv.classList.add("square");
+    newDiv.addEventListener("mouseover", paintSquare);
+    document.querySelector("body").appendChild(newDiv);
+  }
 }
 
-// Change the event that changes your box colors from 'click' to 'mouseover'
-// Paint a picture!
+function paintSquare() {
+  this.style.backgroundColor = color;
+}
