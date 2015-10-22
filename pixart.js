937dc88dbfa89e61f7fc4787db891bbf3e42604d
Kelly Austin <kaustly@gmail.com>
2015-10-22 17:06:51 -0400

    through second step

--- pixart.js
+++ pixart.js
@@ -1,1 +1,19 @@
+$(document).ready(function(){
+  $("#set-color").click(function(evt){
+    evt.preventDefault();
+    console.log("I was clicked");
+    $("#color-field").val();
+    $(".brush").css("background-color", $("#color-field").val());
 
+  })
+
+})
+
+// var Pixart = {
+//   elements: {
+//     var setColor = ('set-color')
+//     ;
+//     var draw =
+//   }
+//   $("body")[0].addEventListener("click",)
+// }
